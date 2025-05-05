3d5ab9526d91b8c04da58acea36033e071466380
cyberpsychozz <nik631stal@mail.ru>
2025-05-05 11:23:17 +0700

    upd Canny

--- CVlibC.h
+++ CVlibC.h
@@ -21,11 +21,15 @@ double Sobel_kernel_X[9] = {
     Padding - Completed
     Median filter - Completed
 	Gaussian filter(Function, that creates Gauss Kernel) - Completed
+    Canny Edge detection - completed
+    RGB to Gray - completed
 
-	Edge detection
+
     smth else from Opencv
     Image resizing
     Binarisation
+    Threshold parameters in Canny filter
+    User friendly variant of main
 */
 
 
@@ -209,7 +213,7 @@ double *generate_gaussian_kernel(int size, double sigma) {
     return kernel;
 }
 
-// проверить на водяные знаки 
+// Function turning 
 unsigned char* rgb_to_grayscale(unsigned char* image, int width, int height, int channels, int *new_width, int *new_height, int *new_channels) {
     *new_width = width;
     *new_height = height;
@@ -242,6 +246,8 @@ int d[16] = {
     1, -1
 };
 
+//Function for recursive following edge
+
 void edge_following(unsigned char *input, unsigned char *output, int width, int height, int x, int y){
    for(int i = 0; i < 16; i += 2){
        int nx = x + d[i];
@@ -260,25 +266,20 @@ void edge_following(unsigned char *input, unsigned char *output, int width, int 
 
 
 
-unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, int channels, int paddig_size, int *new_width_out, int *new_height_out, int *new_channels){
+unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, int channels, int paddig_size, int *new_width_out, int *new_height_out, int *new_channels, double upper_threshold, double lower_threshold){
 
-    //Step 1: Applying 5x5 Gaussian kernel to the image to remove the noise
+    // Step 1: Turning image from RGB to Gray
+    int a,b;
+    unsigned char *gray = rgb_to_grayscale(image, width, height, channels, &a, &b, new_channels);
+    
+
+    //Step 2: Applying 5x5 Gaussian kernel to the image to remove the noise
     int kernel_size = 5;
     double sigma = 2.0;
     double *gaussian_kernel = generate_gaussian_kernel(kernel_size,sigma);
-    unsigned char *blurred = convolution(image, width, height, channels, gaussian_kernel, kernel_size, paddig_size, new_width_out, new_height_out);
+    unsigned char *blurred = convolution(gray, width, height, 1, gaussian_kernel, kernel_size, paddig_size, new_width_out, new_height_out);
     int new_width = *new_width_out; 
     int new_height = *new_height_out;
-
-    //Step 1.5(optional): Turning image from RGB(or RGBA) into Gray
-    if (channels >= 3){
-        int a,b;
-        unsigned char *gray = rgb_to_grayscale(blurred, new_width, new_height, channels, &a, &b, new_channels); 
-        free(blurred);
-        blurred = gray;
-    }
-
-    // return blurred;
 
     // Step 3: Finding intensity gradients(filtering with Sobel kernel)
     int neww;
@@ -286,7 +287,6 @@ unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, 
     unsigned char *Gx = convolution(blurred, new_width, new_height, 1, Sobel_kernel_X, 3, 1, &new_width, &new_height);
     unsigned char *Gy = convolution (blurred, new_width, new_height, 1, Sobel_kernel_Y, 3, 1, &new_width, &new_height);
     free(blurred);
-
 
     unsigned char *G = (unsigned char*)malloc(new_width * new_height * sizeof(unsigned char));
     double *direction = (double*)malloc(new_width * new_height * sizeof(double));
@@ -301,7 +301,7 @@ unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, 
     free(Gy);
 
 
-    // Step 3: Nonmaximum Suppresion
+    // Step 4: Nonmaximum Suppresion
 
     unsigned char *supressed = (unsigned char*)malloc(new_height * new_width * sizeof(unsigned char));
 
@@ -344,21 +344,19 @@ unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, 
     free(G);
     free(direction);
 
-    //Step 4: Double threshold 
+    //Step 5: Double threshold 
 
     for(int i = 0; i < new_width * new_height; i++){
-        if(supressed[i] < 50){
+        if(supressed[i] < lower_threshold){
             supressed[i] = 0;
-        }else if(supressed[i] >= 50 && supressed[i] < 100){
+        }else if(supressed[i] >= lower_threshold && supressed[i] < upper_threshold){
             supressed[i] = 100;
-        }else if(supressed[i] >= 100){
+        }else if(supressed[i] >= upper_threshold){
             supressed[i] = 255;
         }
     }
 
-    // return supressed;
-    
-    //Step 5:Edge tracing by hysteresis
+    //Step 6:Edge tracing by hysteresis
     unsigned char *result = (unsigned char*)calloc(new_width * new_height, sizeof(unsigned char));
 
     // int d{8} = [0, 1, -1];
@@ -371,7 +369,6 @@ unsigned char *Canny_Edge_detector(unsigned char *image, int width, int height, 
             }
         }
     }
-    // printf("pupu");
 
     free(supressed);
     return result;
